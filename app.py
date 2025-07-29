cd3a3c77fc561d5f3848409acd0c1e2a8a126952
ziadwaelai <ziadwael142@gmail.com>
2025-07-29 11:20:29 +0300

    get_context_route

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from LetterToPdf.letterToPdf import LetterPDF
 from dotenv import load_dotenv
 import os
 import threading
-from n8n.app import n8n_get_context
+from n8n.app import n8n_get_context , n8n_get_system_prompt
+from ai_generator import generate_letter_id
 # Load environment variables
 load_dotenv()
 
@@ -164,16 +165,48 @@ def process_letter_in_background(template, letter_content, id, letter_type, reci
 
 @app.route("/get-context", methods=["POST"])
 def get_context_route():
+    letter_id=generate_letter_id()
     data = request.json
     if not data:
         return jsonify({"error": "No JSON data provided"}), 400
     category = data.get("category")
     member_name = data.get("member_name", None)
-    if not category:
-        return jsonify({"error": "Category is required"}), 400
+    recipient = data.get("recipient")
+    is_firstTime = data.get("isFirst")
+    prompt = data.get("prompt")
+    recipient_job_title = data.get("recipient_job_title", "")
+    recipient_title = data.get("recipient_title", "")
+    organization_name = data.get("organization_name", "")
+    previous_letter_content = data.get("previous_letter_content", "")
+    previous_letter_id = data.get("previous_letter_id", "")
+
+    if not category or not prompt or not recipient or is_firstTime is None:
+        return jsonify({"error": "Category, prompt, recipient, and isFirst are required"}), 400
     try:
         context = n8n_get_context(category, member_name)
-        return jsonify(context), 200
+        instruction= context.get("data", {}).get("instruction", "")
+        member_info = context.get("data", {}).get("member_info", "")
+        letter = context.get("data", {}).get("letter", "")
+
+        return jsonify({
+            "status": "success",
+            "data": {
+                "context": context.get("data", {}),
+                "system_prompt": n8n_get_system_prompt(
+                    user_prompt=prompt,
+                    reference_context=letter,
+                    member_info=member_info,
+                    writing_instructions=instruction,
+                    recipient=recipient,
+                    recipient_title=recipient_title,
+                    recipient_job_title=recipient_job_title,
+                    is_first_contact=is_firstTime,
+                    previous_letter_content=previous_letter_content,
+                    previous_letter_id=previous_letter_id,
+                    letter_id=letter_id
+                )
+            }
+        }), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
