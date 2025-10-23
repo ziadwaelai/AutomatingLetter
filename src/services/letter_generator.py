2afe9c8650e970b999d75730245ec3d408933242
ziadwaelai <ziadwael142@gmail.com>
2025-10-23 19:07:30 +0300

    some fixes

--- src/services/letter_generator.py
+++ src/services/letter_generator.py
@@ -9,7 +9,7 @@ from typing import Optional, Dict, Any
 from dataclasses import dataclass
 
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.runnables import Runnable
 
